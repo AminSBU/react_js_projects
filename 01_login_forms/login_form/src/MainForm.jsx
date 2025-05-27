f1902a9c2bc52099f6fd4ec28292fed82e9cf55f
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-27 18:36:06 +0330

    use 2 input for username and password

--- 01_login_forms/login_form/src/MainForm.jsx
+++ 01_login_forms/login_form/src/MainForm.jsx
@@ -1,6 +1,17 @@
 import React, {useState} from "react";
 
 function MainForm(){
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
 
+    return(
+        <>
+            <div className="main-form-container">
+                <h2>Register/Login Form</h2>
+                <input type="text" value={username} placeholder="type your username ..."></input>
+                <input type="password" value={password} placeholder="type your password ..."></input>
+            </div>
+        </>
+    );
 }
 export default MainForm;
