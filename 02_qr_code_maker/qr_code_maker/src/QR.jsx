8ff1fb476a32a904728ff85495b04b9e0e03bd23
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-06-01 18:17:19 +0330

    write qrInputHandler function

--- 02_qr_code_maker/qr_code_maker/src/QR.jsx
+++ 02_qr_code_maker/qr_code_maker/src/QR.jsx
@@ -3,10 +3,18 @@ import React, {use, useState} from "react";
 function QR()
 {
     const[qr, setQR] = useState();
+    const[temp, setTemp] = useState();
+
+    function qrInputHandler(event){
+        setTemp(event.target.value);
+    }
 
     return(
         <>
-            
+            <div>
+                <input type="text" value={qr} onChange={qrInputHandler}></input>
+                <button>make QR</button>
+            </div>
         </>
     );
 }
