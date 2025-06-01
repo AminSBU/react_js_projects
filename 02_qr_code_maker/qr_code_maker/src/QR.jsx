daf390b1a7afcd0e4e5a89ae9c496d94ae412de0
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-06-01 18:22:07 +0330

    add qr address api

--- 02_qr_code_maker/qr_code_maker/src/QR.jsx
+++ 02_qr_code_maker/qr_code_maker/src/QR.jsx
@@ -5,15 +5,23 @@ function QR()
     const[qr, setQR] = useState();
     const[temp, setTemp] = useState();
 
+    fetch('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example')
+   .then(response => response.json()) 
+   .then(data => console.log(data));
+
     function qrInputHandler(event){
         setTemp(event.target.value);
+    }
+
+    function clickHandler(event){
+        setWord(temp)
     }
 
     return(
         <>
             <div>
                 <input type="text" value={qr} onChange={qrInputHandler}></input>
-                <button>make QR</button>
+                <button onClick={clickHandler}>make QR</button>
             </div>
         </>
     );
