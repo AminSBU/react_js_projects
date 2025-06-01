5d55201cdcf40ec7924fd0f3c3dae27e38e50e1e
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-06-01 18:11:20 +0330

    first issue for qr code maker

---
+++ 02_qr_code_maker/qr_code_maker/src/QR.jsx
@@ -0,0 +1,13 @@
+import React, {use, useState} from "react";
+
+function QR()
+{
+    const[qr, setQR] = useState();
+
+    return(
+        <>
+            
+        </>
+    );
+}
+export  default QR;
