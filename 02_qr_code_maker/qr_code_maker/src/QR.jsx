4ea736c0d67aaf5e4ac3ba2b4ab3c5d732e361be
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-06-02 17:42:02 +0330

    solve problem of show qr code

--- 02_qr_code_maker/qr_code_maker/src/QR.jsx
+++ 02_qr_code_maker/qr_code_maker/src/QR.jsx
@@ -1,29 +1,37 @@
-import React, {use, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-function QR()
-{
-    const[qr, setQR] = useState();
-    const[temp, setTemp] = useState();
+function QR() {
+  const [temp, setTemp] = useState("");
+  const [qrUrl, setQrUrl] = useState("");
 
-    fetch('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example')
-   .then(response => response.json()) 
-   .then(data => console.log(data));
+  function qrInputHandler(event) {
+    setTemp(event.target.value);
+  }
 
-    function qrInputHandler(event){
-        setTemp(event.target.value);
-    }
+  function clickHandler() {
+    // Build the QR code URL dynamically using the current input value
+    const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(temp)}`;
+    setQrUrl(url);
+  }
 
-    function clickHandler(event){
-        setWord(temp)
-    }
-
-    return(
-        <>
-            <div>
-                <input type="text" value={qr} onChange={qrInputHandler}></input>
-                <button onClick={clickHandler}>make QR</button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div style={{ marginBottom: "1rem" }}>
+        <input
+          type="text"
+          value={temp}
+          onChange={qrInputHandler}
+          placeholder="Enter text to generate QR"
+        />
+        <button onClick={clickHandler}>Make QR</button>
+      </div>
+      {qrUrl && (
+        <div>
+          <img src={qrUrl} alt="Generated QR code" />
+        </div>
+      )}
+    </>
+  );
 }
-export  default QR;
+
+export default QR;
