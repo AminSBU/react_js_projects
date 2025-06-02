9a3df812405bf652d64ed645e26d8e6f8ac8e31a
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-02 21:58:42 +0330

    use persian numbers, add some css file for number

--- 03_weather_date_app/weather_date_app/src/Application.jsx
+++ 03_weather_date_app/weather_date_app/src/Application.jsx
@@ -1,6 +1,20 @@
 import React, {useEffect, useState} from "react";
+import './Application.css'
+
+const englishToPersianDigits = (str) => {
+    const persianDigits = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+    return str.replace(/\d/g, (digit) => persianDigits[digit]);
+};
 
 function Application(){
+    const number = 36;
 
+    return(
+        <>
+            <div className="app-container">
+                <p className="number-style">{englishToPersianDigits(number.toString())}</p>
+            </div>
+        </>
+    );
 }
 export default Application;
