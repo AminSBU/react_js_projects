0029df661610179500ff5d29e621c6f3fc75c7c7
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-06-03 19:02:10 +0330

    connect to weather api, show image

--- 02_qr_code_maker/qr_code_maker/src/QR.jsx
+++ 02_qr_code_maker/qr_code_maker/src/QR.jsx
@@ -8,6 +8,10 @@ function QR() {
     setTemp(event.target.value);
   }
 
+const apiKey = 'YOUR_API_KEY';
+const city = 'London';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   function clickHandler() {
     // Build the QR code URL dynamically using the current input value
     const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(temp)}`;
--- 03_weather_date_app/weather_date_app/src/Application.jsx
+++ 03_weather_date_app/weather_date_app/src/Application.jsx
@@ -6,13 +6,45 @@ const englishToPersianDigits = (str) => {
     return str.replace(/\d/g, (digit) => persianDigits[digit]);
 };
 
+
+
 function Application(){
     const number = 36;
 
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=a51baaa6f2214379b36212509252203&q=Tehran&aqi=no`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setWeather(data);
+        console.log('Weather forecast:', data);
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
+
     return(
         <>
-            <div className="app-container">
-                <p className="number-style">{englishToPersianDigits(number.toString())}</p>
+            
+
+            <div className="weather-container">
+                {weather ? (
+                <>
+                    <p>Location: {weather.location.name}</p>
+                    <p>Temperature: {weather.current.temp_c} °C</p>
+                    <p>Condition: {weather.current.condition.text}</p>
+
+                    {weather.current.temp_c < 30 ? (
+                        <img src='./img/partly_cloudy_weather_with_the_sun.png' className="pcws"/>
+                    ) : (
+                        <img src='./img/showers_with_sun.png' className="pcws" />
+                    )}
+                </>
+                ) : (
+                <p>Loading weather...</p>
+                )}
             </div>
         </>
     );
