f98c794465fdf55fa4870277ade6b7c9dd92c026
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-15 21:52:41 +0330

    consider a counter app with value

--- 04_counter_app/counter_app/src/counter.jsx
+++ 04_counter_app/counter_app/src/counter.jsx
@@ -1,6 +1,15 @@
 import React, {useState} from "react";
 
 function Counter(){
-    
+    const[count, setCount] = useState(0);
+
+    return(
+        <>
+            <div className="counter-app-container">
+                <h2>Counter App</h2>
+                <p>{count}</p>
+            </div>
+        </>
+    );
 }
 export default Counter;
