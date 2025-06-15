0463686d5a0c86d463f77837d9bad2bf26e3f78a
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-15 22:35:36 +0330

    create timer app

---
+++ 04_counter_app/counter_app/src/timer.jsx
@@ -0,0 +1,12 @@
+import React, {userState} from "react";
+
+function Timer(){
+    return(
+        <>
+            <div className="timer-app-container">
+                <h2>Timer App</h2>
+            </div>
+        </>
+    );
+}
+export default Timer;
