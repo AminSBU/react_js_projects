2cd0befc811f6b4c04f828b59e31a522bddc279c
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-15 22:06:19 +0330

    write 3 function to increase decrease and reset counter

--- 04_counter_app/counter_app/src/counter.jsx
+++ 04_counter_app/counter_app/src/counter.jsx
@@ -3,15 +3,34 @@ import React, {useState} from "react";
 function Counter(){
     const[count, setCount] = useState(0);
 
+    function increaseHandle(){
+        setCount(count + 1);
+    }
+
+    function decreaseHandle(){
+        if(count > 0)
+        {
+            setCount(count - 1);
+        }
+        else
+        {
+            console.error("counter is less than 0");
+        }
+    }
+
+    function resetHandle(){
+        setCount(0);
+    }
+
     return(
         <>
             <div className="counter-app-container">
                 <h2>Counter App</h2>
                 <p>{count}</p>
                 <div className="counter-buttons">
-                    <button>+</button>
-                    <button>-</button>
-                    <button>R</button>
+                    <button onClick={increaseHandle}>+</button>
+                    <button onClick={decreaseHandle}>-</button>
+                    <button onClick={resetHandle}>R</button>
                 </div>
             </div>
         </>
