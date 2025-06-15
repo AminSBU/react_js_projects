e898890c8c38bebc870ece199dcc4423e7b6910e
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-15 23:09:44 +0330

    write 3 button and useEffect

--- 04_counter_app/counter_app/src/timer.jsx
+++ 04_counter_app/counter_app/src/timer.jsx
@@ -1,10 +1,22 @@
-import React, {userState} from "react";
+import React, {useEffect, userState, useState} from "react";
 
 function Timer(){
+    const[isRunning, SetIsRunning] = useState(false);
+
+    useEffect(() => {
+        let timer;
+        if(isRunning){
+            
+        }
+    });
+
     return(
         <>
             <div className="timer-app-container">
                 <h2>Timer App</h2>
+                <button onClick={() => SetIsRunning(true)}>Start</button>
+                <button onClick={() => SetIsRunning(false)}>Pause</button>
+                <button onClick={() => SetIsRunning(false)}>Reset</button>
             </div>
         </>
     );
