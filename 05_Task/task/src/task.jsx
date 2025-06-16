45a86edb3eed8f149b851b6950c81e9495eaadae
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-16 23:36:15 +0330

    make a border

--- 05_Task/task/src/task.jsx
+++ 05_Task/task/src/task.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
+import './task.css'
 
 function Task(){
     return(
         <>
-            
+            <div className="task-container">
+                
+            </div>
         </>
     );
 }
