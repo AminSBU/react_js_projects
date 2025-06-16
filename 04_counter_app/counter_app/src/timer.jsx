e5bb9d9ab909e3b86e601baf59d795dcad939cb0
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-16 23:28:08 +0330

    create task app

--- 04_counter_app/counter_app/src/timer.jsx
+++ 04_counter_app/counter_app/src/timer.jsx
@@ -1,19 +1,22 @@
 import React, {useEffect, userState, useState} from "react";
 
 function Timer(){
+    const[timer, SetTimer] = useState(0);
     const[isRunning, SetIsRunning] = useState(false);
 
     useEffect(() => {
         let timer;
         if(isRunning){
-            
-        }
+            timer = setInterval(() =>
+            setTime((prevTime) => prevTime + 1);
+        }, 1000);
     });
 
     return(
         <>
             <div className="timer-app-container">
                 <h2>Timer App</h2>
+                <p>{timer}</p>
                 <button onClick={() => SetIsRunning(true)}>Start</button>
                 <button onClick={() => SetIsRunning(false)}>Pause</button>
                 <button onClick={() => SetIsRunning(false)}>Reset</button>
