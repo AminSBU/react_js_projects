a766943e39df66b5e5f0086d44da7c8e62afdeba
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-06-17 19:50:15 +0330

    write input and send button

--- 05_Task/task/src/task.jsx
+++ 05_Task/task/src/task.jsx
@@ -2,10 +2,16 @@ import React, {useState, useEffect} from "react";
 import './task.css'
 
 function Task(){
+    const[task, setTask] = useState();
+    const[input, setInput] = useState();
+
     return(
         <>
             <div className="task-container">
-                
+                <p>دستنویس</p>
+
+                <input type="text" className="task-input"></input>
+                <button className="task-send-button">ارسال</button>
             </div>
         </>
     );
