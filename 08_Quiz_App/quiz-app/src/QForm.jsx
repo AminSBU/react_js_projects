b99ba93de7e89afd83de6716979bcdc8ce7ae4cc
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-07-26 19:31:43 +0330

    make some design for buttons in css

--- 08_Quiz_App/quiz-app/src/QForm.jsx
+++ 08_Quiz_App/quiz-app/src/QForm.jsx
@@ -5,7 +5,13 @@ function QForm()
 {
     return(
         <>
-            <div className="quiz_container"> </div>
+            <div className="quiz_container"> 
+                <p className="quiz-question">Test</p>
+                <div className="question"><button className="qbtn">1. Tokyo</button></div>
+                <div className="question"><button className="qbtn">2. Tehran</button></div>
+                <div className="question"><button className="qbtn">3. Seoul</button></div>
+                <div className="question"><button className="qbtn">4. Bnghkok</button></div>
+            </div>
         </>
     );
 }
