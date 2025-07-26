5ca7b437efb36345b468469a0fa0c42b131d6e2c
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-07-26 19:13:06 +0330

    first structure

---
+++ 08_Quiz_App/quiz-app/src/QForm.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import './QForm.css'
+
+function QForm()
+{
+    return(
+        <>
+            <h1> this is Quiz App</h1>
+        </>
+    );
+}
+export default QForm;
