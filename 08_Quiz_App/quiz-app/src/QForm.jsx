b3e914882ecadd3fa4f919b451a393f678636aa8
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-07-27 17:33:47 +0330

    some modifications

--- 08_Quiz_App/quiz-app/src/QForm.jsx
+++ 08_Quiz_App/quiz-app/src/QForm.jsx
@@ -3,16 +3,39 @@ import './QForm.css'
 
 function QForm()
 {
-    return(
-        <>
-            <div className="quiz_container"> 
-                <p className="quiz-question">Test</p>
-                <div className="question"><button className="qbtn">1. Tokyo</button></div>
-                <div className="question"><button className="qbtn">2. Tehran</button></div>
-                <div className="question"><button className="qbtn">3. Seoul</button></div>
-                <div className="question"><button className="qbtn">4. Bnghkok</button></div>
-            </div>
-        </>
-    );
+  const CorrectAnswer = "Tokyo";
+
+  return (
+    <>
+      <div className="quiz_container">
+        <p className="quiz-question">What is the capital of Japan?</p>
+        
+        <div className="question">
+          <button className="qbtn">
+            1. Tokyo
+          </button>
+        </div>
+
+        <div className="question">
+          <button className="qbtn">
+            2. Tehran
+          </button>
+        </div>
+
+        <div className="question">
+          <button className="qbtn">
+            3. Seoul
+          </button>
+        </div>
+
+        <div className="question">
+          <button className="qbtn">
+            4. Bnghkok
+          </button>
+        </div>
+
+      </div>
+    </>
+  );
 }
 export default QForm;
