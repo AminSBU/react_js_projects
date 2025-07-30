ca3c83fe25027c6a6910aacc9275549b5e2ed75d
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-07-30 18:10:43 +0330

    use image for header

--- 06_header/header/src/header.jsx
+++ 06_header/header/src/header.jsx
@@ -6,9 +6,10 @@ function Header()
     return(
     <>
         <div class="parent">
-            <div class="div1"><button>home</button></div>
-            <div class="div2"><button>Contact us</button></div>
-            <div class="div3"><button>About us</button></div>
+            <div className="img-header"><img src="src/img/header.jpg" className="img-header"></img></div>
+            <div class="div1"><button className="header-btn">home</button></div>
+            <div class="div2"><button className="header-btn">Contact us</button></div>
+            <div class="div3"><button className="header-btn">About us</button></div>
         </div>
     </>
     );
