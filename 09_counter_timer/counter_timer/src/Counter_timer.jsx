f530295045435e91941f05c2bce9b5bf09f6855c
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-08-03 19:54:32 +0330

    some modification

--- 09_counter_timer/counter_timer/src/Counter_timer.jsx
+++ 09_counter_timer/counter_timer/src/Counter_timer.jsx
@@ -2,18 +2,29 @@ import React, {useState} from "react";
 
 function Counter_Timer()
 {
-    const[counter, setCounter] = useState(0);
+    const[second, setSecond] = useState(0);
 
     const StartInterval = () => {
-        setInterval(() => {
-            setCounter(prevcounter => prevcounter + 1)
+        let secValue = setInterval(() => {
+            setSecond(prevcounter => prevcounter + 1)
         }, 1000);
+
+        if(secValue >= 15)
+        {
+            clearInterval(secValue)
+            setSecond(0);
+        }
+    };
+
+    const StoptInterval = () => {
+        setSecond(0);
     };
 
     return(<>
         <div className="items_container">
-            <p>{counter}</p>
-            <button onClick={StartInterval}></button>
+            <p>{second}</p>
+            <button onClick={StartInterval}>Start</button>
+            <button onClick={StoptInterval}>Stop</button>
         </div>
     </>);
 }
